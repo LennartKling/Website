8118252e0a89b22a0593d33954572bdd9f482e49
KameterLuis <contact@luiskameter.com>
2023-12-02 10:10:41 +0100

    update

---
+++ pages/api/SwipeMotion.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const SwipeMotion = () => {
+  let down: any = null;
+
+  const [swipe, setSwipe] = useState(0);
+
+  const updateTouchStart = (e: any) => {
+    down = e.clientY;
+  };
+
+  const updateTouchMove = (e: any) => {
+    if (!down) return;
+    let up = e.clientY;
+    updateSwipe(up - down);
+    down = null;
+  };
+
+  const updateSwipe = (y: number) => {
+    setSwipe(y);
+  };
+
+  useEffect(() => {
+    document.addEventListener("touchstart", updateTouchStart);
+    document.addEventListener("touchmove", updateTouchMove);
+    return () => {
+      document.removeEventListener("touchstart", updateTouchStart);
+      document.removeEventListener("touchmove", updateTouchMove);
+    };
+  }, []);
+
+  return swipe;
+};
+
+export default SwipeMotion;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,12 @@
 import Head from "next/head";
-import Navbar from "../components/navbar/Navbar";
-import Hero from "../components/landing/hero/Hero";
-import Products from "../components/landing/products/Products";
-import Partners from "../components/landing/partners/Partners";
-import Features from "../components/landing/features/Features";
-import Footer from "../components/footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Leam</title>
-        <link rel="icon" href="/assets/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
-      <Navbar />
-      <Hero />
-      <Products />
-      <Partners />
-      <Features />
-      <Footer />
     </div>
   );
 }
