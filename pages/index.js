12716f0873158c4e594a018ba3be7abfc4ee6b7c
KameterLuis <152624618+KameterLuis@users.noreply.github.com>
2024-08-20 16:16:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ export default function Home() {
         <Navbar />
         <Hero />
         <Info />
-        <Faq />
         <Downloads />
+        <Faq />
         <Contact />
         <Footer />
       </div>
