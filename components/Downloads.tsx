4758f19d17927e0a6071510f22160c64b02303c4
KameterLuis <152624618+KameterLuis@users.noreply.github.com>
2024-08-20 16:16:36 +0200

    Update Downloads.tsx

--- components/Downloads.tsx
+++ components/Downloads.tsx
@@ -4,24 +4,24 @@ const Downloads = () => {
         <p className="text-5xl">Downloads</p>
         <div className="mt-10">
           <p className="mt-8 text-2xl font-semibold">
-            Übersicht Alltagskleidung - Jacken, Jogger & co.
-          </p>
-          <div className="mt-4">
-            <a
-              className="underline block text-xl"
-              href="/files/Alltagskleidung-Jacken,Jogger&co.pdf"
-              download
-            >
-              Herunterladen
-            </a>
-          </div>
-          <p className="mt-8 text-2xl font-semibold">
             Übersicht Klassiker - Hoodies, Zipper, Shirts
           </p>
           <div className="mt-4">
             <a
               className="underline block text-xl"
               href="/files/KLASSIKER-Hoodies,Zipper,Shirts.pdf"
+              download
+            >
+              Herunterladen
+            </a>
+          </div>
+            <p className="mt-8 text-2xl font-semibold">
+            Übersicht Alltagskleidung - Jacken, Jogger & co.
+          </p>
+          <div className="mt-4">
+            <a
+              className="underline block text-xl"
+              href="/files/Alltagskleidung-Jacken,Jogger&co.pdf"
               download
             >
               Herunterladen
